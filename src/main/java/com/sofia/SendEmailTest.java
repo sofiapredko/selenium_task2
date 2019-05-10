e8f11bbca3592a1366e3aaf49718ce34b5ea117b
Sofia Predko <sophiepredko@gmail.com>
2019-05-10 15:43:34 +0300

    First basic version

---
+++ src/main/java/com/sofia/SendEmailTest.java
@@ -0,0 +1,84 @@
+package com.sofia;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class SendEmailTest {
+    private static final Logger LOG = LogManager.getLogger(SendEmailTest.class);
+    private static final String SIGN_IN_PAGE = "https://mail.google.com";
+    private static final String DRIVER_PATH = "src/main/resources/chromedriver.exe";
+    private static final String WEB_DRIVER_NAME = "webdriver.chrome.driver";
+    private static final String TEST_USERNAME = "sonyachanter@gmail.com";
+    private static final String TEST_PASSWORD = "sonichka13";
+    private static WebDriver driver;
+
+    static {
+        System.setProperty(WEB_DRIVER_NAME, DRIVER_PATH);
+        driver = new ChromeDriver();
+    }
+
+    @BeforeTest
+    public void setObjects() {
+        driver.manage().timeouts()
+                .implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void logInAndSendEmail() {
+        driver.get(SIGN_IN_PAGE);
+        WebElement signInEmail = driver.findElement(By.xpath("//input[@id='identifierId']"));
+        signInEmail.sendKeys(TEST_USERNAME);
+        signInEmail.sendKeys(Keys.ENTER);
+
+        WebElement activeEmailUser = driver.findElement(By.id("profileIdentifier"));
+        assertEquals(activeEmailUser.getAttribute("innerHTML"), TEST_USERNAME);
+        LOG.info("Username Correct");
+
+        WebElement signInPassword = driver.findElement(By.name("password"));
+        signInPassword.sendKeys(TEST_PASSWORD);
+        signInPassword.sendKeys(Keys.ENTER);
+
+        LOG.info("Sign in successfully!");
+
+        WebElement writeEmailButton = driver.findElement(By.xpath("//div[text()='Написати']"));
+        writeEmailButton.click();
+
+        WebElement toPersonTextArea = driver.findElement(By.name("to"));
+        toPersonTextArea.click();
+        toPersonTextArea.sendKeys("sophiepredko@gmail.com");
+
+        WebElement themeArea = driver.findElement(By.name("subjectbox"));
+        themeArea.click();
+        String subj = "Test Email";
+        themeArea.sendKeys(subj);
+
+        //assertEquals(themeArea.getAttribute("dir"), subj);
+
+        WebElement writeEmailTextArea = driver.findElement(By.xpath("//div[@role='textbox']"));
+        writeEmailTextArea.click();
+        writeEmailTextArea.sendKeys("This is test email :) ");
+
+        WebElement sendButton = driver.findElement(By.xpath("//div[@data-tooltip='Надіслати ‪(Ctrl –Enter)‬']"));
+        sendButton.click();
+
+//      assertTrue(imagePageElement.getAttribute("aria-selected"), true);
+    }
+
+    @AfterTest
+    public void endTest() {
+        driver.quit();
+    }
+
+}
